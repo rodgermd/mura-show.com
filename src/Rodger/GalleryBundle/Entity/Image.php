c617039f168a8faa6231990ce3dcf748731ae4ac
rodger <rodger@rodger-HP>
2011-11-13 21:58:19 +0200

    GalleryBundle entities

---
+++ src/Rodger/GalleryBundle/Entity/Image.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Rodger\GalleryBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Rodger\GalleryBundle\Entity\Image
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Rodger\GalleryBundle\Entity\ImageRepository")
+ */
+class Image
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @var string $filename
+     *
+     * @ORM\Column(name="filename", type="string", length=50)
+     */
+    private $filename;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set filename
+     *
+     * @param string $filename
+     */
+    public function setFilename($filename)
+    {
+        $this->filename = $filename;
+    }
+
+    /**
+     * Get filename
+     *
+     * @return string 
+     */
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+}
