2039ba2cf87ce2de6e7b5825dc929f3825910a01
rodger <rodger@rodger-HP>
2011-11-13 21:55:27 +0200

    GalleryBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Rodger\UserBundle\RodgerUserBundle(),
+            new Rodger\GalleryBundle\RodgerGalleryBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Rodger/GalleryBundle/RodgerGalleryBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rodger\GalleryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RodgerGalleryBundle extends Bundle
+{
+}
