b4c9cfab9a418f7a4d502aa83bf0e2ccd403a934
rodger <rodger@local>
2011-12-03 23:08:03 +0200

    Drop avalanche imagine

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Rodger\UserBundle\RodgerUserBundle(),
             new Rodger\GalleryBundle\RodgerGalleryBundle(),
+            
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
