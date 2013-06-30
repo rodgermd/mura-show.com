3582c3af3ceafc46fc513968f29cd5a35bc34d12
Alexander Murashov <rodger@ladela.com>
2013-06-30 12:23:37 +0300

    Fix album edit

--- src/Rodger/GalleryBundle/Resources/public/js/album.edit.js
+++ src/Rodger/GalleryBundle/Resources/public/js/album.edit.js
@@ -4,28 +4,16 @@ $.album_edit = function () {
 
   var $obj = {
     init: function () {
-      $("#navigation a", $holder).live('click', $obj.load_page)
       $('#images-list', $holder).use_select_all();
       $images_upload_form.fileupload({
         url: $images_upload_form.attr('action')
       });
-    },
-    load_page: function (e) {
-      e.preventDefault();
-      $obj.load($(this).attr('href'));
-    },
-    load: function (url) {
-      $.ajax({
-        url: url,
-        success: function (r) {
-          $holder.html($(r).html());
-          $('#images-list', $holder).use_select_all();
-        }
-      });
     }
-
   };
 
   $obj.init();
 };
 
+$(function () {
+  $.album_edit()
+});
