4a38ace0850ae9e5c4333480d2aaa97df2f89c5c
Alexander Murashov <rodger@ladela.com>
2013-11-14 15:00:57 +0200

    Bootstrap fixes

--- rsync-from-production.sh
+++ rsync-from-production.sh
@@ -1,12 +1,12 @@
 echo "copy uploads"
-rsync -vzru  rodger@home.mura-show.com:/var/www/rodger/www.mura-show.com/uploads/images uploads/ --progress --partial
+rsync -vzru  rodger@www.mura-show.com:/var/www/rodger/www.mura-show.com/uploads/images uploads/ --progress --partial
 echo "copy thumbnails"
-rsync -vzru  rodger@home.mura-show.com:/var/www/rodger/www.mura-show.com/web/gallery web/ --progress --partial
+rsync -vzru  rodger@www.mura-show.com:/var/www/rodger/www.mura-show.com/web/gallery web/ --progress --partial
 echo "prepare dump"
-ssh rodger@home.mura-show.com 'cd /var/www/rodger/www.mura-show.com && mysqldump -umura_show -pmuZUFwDx2B2tQfa8 www_mura_show_com --add-drop-table > dump.sql';
+ssh rodger@www.mura-show.com 'cd /var/www/rodger/www.mura-show.com && mysqldump -umura_show -pmuZUFwDx2B2tQfa8 www_mura_show_com --add-drop-table > dump.sql';
 echo "copy dump here"
-scp rodger@home.mura-show.com:/var/www/rodger/www.mura-show.com/dump.sql .
+scp rodger@www.mura-show.com:/var/www/rodger/www.mura-show.com/dump.sql .
 echo "drop dump there"
-ssh rodger@home.mura-show.com 'cd /var/www/rodger/www.mura-show.com && rm -f dump.sql'
+ssh rodger@www.mura-show.com 'cd /var/www/rodger/www.mura-show.com && rm -f dump.sql'
 echo "load dump"
-mysql -uroot -pparty3an murashow < dump.sql
+mysql -uroot -pparty3an rodger__mura_show < dump.sql
--- src/Rodger/UserBundle/RodgerUserBundle.php
+++ src/Rodger/UserBundle/RodgerUserBundle.php
@@ -6,8 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class RodgerUserBundle extends Bundle
 {
-//  public function getParent()
-//  {
-//    return 'FOSUserBundle';
-//  }
+  public function getParent()
+  {
+    return 'FOSUserBundle';
+  }
 }
