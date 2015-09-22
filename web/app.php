2822253908ea1a4974958048f60c8e632c2bb681
rodger <rodger@mura-show.com>
2015-09-22 23:51:58 +0300

    update app.php

--- web/app.php
+++ web/app.php
@@ -1,14 +1,27 @@
 <?php
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
-require_once __DIR__.'/../app/AppKernel.php';
-require_once __DIR__.'/../app/AppCache.php';
-
-umask(0002);
-
+use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
+
+umask(0000);
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+// Use APC for autoloading to improve performance
+// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
+/*
+$loader = new ApcClassLoader('sf2', $loader);
+$loader->register(true);
+*/
+
+require_once __DIR__.'/../app/AppKernel.php';
+//require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-$kernel = new AppCache($kernel);
-$kernel->handle(Request::createFromGlobals())->send();
+//$kernel = new AppCache($kernel);
+$request = Request::createFromGlobals();
+$request->enableHttpMethodParameterOverride();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
